37d3039806330722d95aac6dd6f87f5b47942c0c
jf678-cornell <138830741+jf678-cornell@users.noreply.github.com>
2025-07-02 08:55:03 -0600

    Adding PI T0 T1 T2, as well as T2 calculating function

---
+++ Supplementary_code/Tools/calculateT2.m
@@ -0,0 +1,23 @@
+function T1 = calculateT2(T_matrix)
+
+size_of_T_matrix = size(T_matrix);
+if length(size_of_T_matrix) == 2
+    size_of_T_matrix = [size_of_T_matrix 2];
+    T_matrix = cat(3,T_matrix,T_matrix);
+end
+
+T_zonal = squeeze(mean(T_matrix,1));
+
+T1 = zeros(size_of_T_matrix(3),1);
+load get_lat_and_lon.mat
+
+
+A = 2*pi*earthRadius^2;
+
+fun = @(j) T_zonal(j,i)*cosd(lat(j))*sind(lat(j));
+j = 1:192';
+for i = 1:size_of_T_matrix(3)
+   T1(i) = sum(T_zonal(j,i).*cosd(lat(j)).*(3*sind(lat(j)).^2-1)/2)/sum(cosd(lat(j)));
+end
+
+end
